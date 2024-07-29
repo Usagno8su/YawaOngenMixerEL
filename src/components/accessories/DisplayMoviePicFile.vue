eb790f811e9ce256897116b5e0c91650a137fff9
Usagno8su <120317207+Usagno8su@users.noreply.github.com>
2024-07-30 00:41:32 +0900

    立ち絵画像の表示が更新されない問題を修正

--- src/components/accessories/DisplayMoviePicFile.vue
+++ src/components/accessories/DisplayMoviePicFile.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 }>()
 // 立ち絵画像のみ変換を行って表示する
 
-import type { outSettingType, infoSettingType } from '@/type/data-type'
+import type { outSettingType, infoSettingType, encodeProfileSendReType } from '@/type/data-type'
 import { ref, onUnmounted } from 'vue'
 import { enterEncodeTatiePicFile, enterSaveUint8ArrayFileData } from '@/utils/analysisFile'
 import { DEFAULT_KYARA_TATIE_UUID } from '@/data/data'
@@ -24,16 +24,14 @@ const runMakeImg = ref<boolean>(true)
 // 変換した立ち絵画像を取得
 const img = ref<string | ArrayBuffer>()
 const data = ref<{ buffer: Uint8Array; path: string }>({ buffer: undefined, path: '' })
-const getKyaraImg = async (sta: string) => {
+const getKyaraImg = async (profile: encodeProfileSendReType) => {
   // 立ち絵があり、他の画像取得が動作していない場合のみ実施
-  if (props.selectTatieFile !== DEFAULT_KYARA_TATIE_UUID && runMakeImg) {
+  if (profile.settingList.tatie.tatieUUID.val !== DEFAULT_KYARA_TATIE_UUID && runMakeImg) {
     // 二重起動しないように値を変更
     runMakeImg.value = false
 
     // 立ち絵画像を変換して取得
-    data.value = await enterEncodeTatiePicFile(
-      createVoiceFileEncodeSetting(props.dateList[props.index], props.index, props.dateList, props.infoData),
-    )
+    data.value = await enterEncodeTatiePicFile(profile)
 
     let bobData = new Blob([data.value.buffer], { type: 'image/png' })
     // ファイreaderを作成
@@ -61,17 +59,22 @@ const saveImg = async () => {
   }
 }
 
-getKyaraImg(props.selectTatieFile)
+// コンポーネント表示時に、立ち絵画像の表示をも行う
+const profile = ref<encodeProfileSendReType>(
+  createVoiceFileEncodeSetting(props.dateList[props.index], props.index, props.dateList, props.infoData),
+)
+getKyaraImg(profile.value)
+const checkConf = ref<string>(JSON.stringify(profile.value.settingList.tatie, undefined, 2))
 
-// 立ち絵の設定が変わったら表示を変更する
-const checkConf = ref<string>(JSON.stringify(props.dateList[props.index].tatie, undefined, 2))
+// 指定時間ごとに確認し、立ち絵の設定が変わったら表示を変更する
 const onEncodeTatie = setInterval(() => {
   // 比較のために設定内容をJSON形式に変換
-  const ans = JSON.stringify(props.dateList[props.index].tatie, undefined, 2)
+  profile.value = createVoiceFileEncodeSetting(props.dateList[props.index], props.index, props.dateList, props.infoData)
+  const ans = JSON.stringify(profile.value.settingList.tatie, undefined, 2)
 
   // 比較して前回の内容と異なっていれば立ち絵画像の表示を更新する。
   if (checkConf.value !== ans) {
-    getKyaraImg(props.selectTatieFile)
+    getKyaraImg(profile.value)
     checkConf.value = ans
   }
 }, 2000)
