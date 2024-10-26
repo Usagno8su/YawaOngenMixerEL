0cb3d1e3c7676b005ae43ce21227f1192d682d0b
Usagno8su <120317207+Usagno8su@users.noreply.github.com>
2024-10-26 20:21:49 +0900

    全体設定の項目を追加 #1494

--- src/components/accessories/GlobalSetting/MainSetting.vue
+++ src/components/accessories/GlobalSetting/MainSetting.vue
@@ -48,16 +48,29 @@ watch(
       </button>
     </div>
     <div class="m-1">
-      <div>
-        <div class="mt-3 text-xl font-medium">FFmpegのパス</div>
-        <div class="mx-2 mt-1 border border-gray-600 p-1">
-          <input class="w-full" v-model="globalSettingList.exeFilePath.ffmpeg" @keydown.enter="saveEnter()" />
+      <div class="mt-1 bg-gray-100 py-2">
+        <div class="text-2xl">外部ソフトの設定</div>
+        <div>
+          <div class="mt-3 text-xl font-medium">FFmpegのパス</div>
+          <div class="mx-2 mt-1 border border-gray-600 p-1">
+            <input class="w-full" v-model="globalSettingList.exeFilePath.ffmpeg" @keydown.enter="saveEnter()" />
+          </div>
+        </div>
+        <div>
+          <div class="mt-3 text-xl font-medium">ImageMagicのパス</div>
+          <div class="mx-2 mt-1 border border-gray-600 p-1">
+            <input class="w-full" v-model="globalSettingList.exeFilePath.convert" @keydown.enter="saveEnter()" />
+          </div>
         </div>
       </div>
-      <div>
-        <div class="mt-3 text-xl font-medium">ImageMagicのパス</div>
-        <div class="mx-2 mt-1 border border-gray-600 p-1">
-          <input class="w-full" v-model="globalSettingList.exeFilePath.convert" @keydown.enter="saveEnter()" />
+      <div class="mt-1 bg-gray-100 py-2">
+        <div class="text-2xl">表示設定</div>
+
+        <div class="mt-2 flex justify-between bg-gray-200 py-2">
+          <div class="mx-2 my-1 text-xl font-medium">ファイルリストで字幕の内容を表示</div>
+          <div class="mx-2 mt-1 flex items-center p-1">
+            <input class="" type="checkbox" />
+          </div>
         </div>
       </div>
     </div>
