a37ab46822b9f8ea151198ec5cce5689ceb527cf
Usagno8su <120317207+Usagno8su@users.noreply.github.com>
2024-12-13 21:18:13 +0900

    音声ファイル個別の設定の設定範囲の項目にマウスを置くと、ファイル名を確認できるようにします。 #1504

--- src/components/accessories/DisplaySelectFileView.vue
+++ src/components/accessories/DisplaySelectFileView.vue
@@ -10,7 +10,11 @@ import type { outSettingType } from '@/type/data-type'
   <div class="flex items-center font-bold">
     <div class="mr-1 border-r-[1px] border-gray-400 pr-1 font-normal text-gray-600">設定範囲</div>
     <div v-if="dateList[selectKyara].dataType === 'defo'" class="font-bold text-red-500">全体デフォルト設定</div>
-    <div v-else-if="dateList[selectKyara].dataType === 'seid'" class="font-bold text-red-600">
+    <div
+      v-else-if="dateList[selectKyara].dataType === 'seid'"
+      class="font-bold text-red-600"
+      :title="dateList[selectKyara].fileName + '.' + dateList[selectKyara].fileExtension"
+    >
       {{ dateList[selectKyara].fileName }}
     </div>
     <div v-else class="flex items-center">
