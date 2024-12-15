ced9c3a1144740911964af2e5194eea9f5ad1d1f
Usagno8su <120317207+Usagno8su@users.noreply.github.com>
2024-12-15 16:49:21 +0900

    キャラ設定のコピーや削除の選択を行う右クリックメニューを追加 #1500

---
+++ src/components/accessories/SelectDisplayKyaraRightClickMenu.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps<{
+  clickClose: () => void
+  editDataClik: () => void
+  askDeleteKyara: () => void
+}>()
+// キャラ設定のコピーや削除の選択を行う右クリックメニュー
+</script>
+
+<template>
+  <div
+    class="fixed right-0 top-0 flex h-screen w-screen items-center justify-center bg-gray-600 bg-opacity-30"
+    @click.self="clickClose"
+  >
+    <!-- 画面外クリックでクローズ -->
+  </div>
+  <div
+    class="border-1 fixed top-1/3 flex w-96 flex-col rounded-3xl border-gray-800 bg-blue-100 px-5 py-4 text-sm"
+    @click.stop="clickClose"
+  >
+    <button
+      @click="console.log('キャラ設定にコピー')"
+      class="flex justify-between bg-blue-100 px-2 py-1 text-gray-700 hover:bg-blue-600 hover:text-gray-200"
+    >
+      <div>キャラ設定にコピー</div>
+      <div>Ctrl+E</div>
+    </button>
+    <button
+      @click="console.log('スタイル付きキャラ設定にコピー')"
+      class="flex justify-between bg-blue-100 px-2 py-1 text-gray-700 hover:bg-blue-600 hover:text-gray-200"
+    >
+      <div>スタイル付きキャラ設定にコピー</div>
+      <div>Ctrl+R</div>
+    </button>
+    <button
+      @click="editDataClik()"
+      class="flex justify-between bg-blue-100 px-2 py-1 text-gray-700 hover:bg-blue-600 hover:text-gray-200"
+    >
+      <div>名前の変更</div>
+      <div>Ctrl+Space</div>
+    </button>
+    <button
+      @click="askDeleteKyara()"
+      class="flex justify-between bg-blue-100 px-2 py-1 text-gray-700 hover:bg-blue-600 hover:text-gray-200"
+    >
+      <div>削除</div>
+      <div>Ctrl+Alt+D</div>
+    </button>
+  </div>
+</template>
