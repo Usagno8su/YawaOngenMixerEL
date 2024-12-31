dd5f182204e4036eae0bdad266d175aefc20c048
Usagno8su <120317207+Usagno8su@users.noreply.github.com>
2024-12-31 17:00:19 +0900

    MakeClassString()を使用するように修正 #1456

--- src/components/accessories/SelectDisplayHigherUpStatus.vue
+++ src/components/accessories/SelectDisplayHigherUpStatus.vue
@@ -7,29 +7,28 @@ const props = defineProps<{
 }>()
 
 // 選択中のキャラ設定で、その項目を有効にするか選択するボタン
+// ONのときはそのキャラ設定タイプ
 // OFFのときはどの上位設定が使われているか色で表示する。
 
 import type { dataTextType } from '@/type/data-type'
 import { typeColor } from '@/data/data'
-
-// ボタンの色を決める
-// ONのときはそのキャラ設定タイプ
-// OFFのときは使用する上位設定の色にする
-const selectTypeColor = (): string => {
-  return props.actStatus
-    ? typeColor[props.settype].bg + ' ' + 'border-gray-800'
-    : typeColor[props.higherUpType].bg + ' ' + 'text-gray-500 border-gray-500'
-}
-
-// 選択中の要素の場合は背景色を変更する
-const actset = (): string => {
-  return 'px-1 border rounded-full w-10 font-medium hover:bg-gray-600 hover:text-gray-100' + ' ' + selectTypeColor()
-}
+import { MakeClassString } from '@/utils/analysisGeneral'
 </script>
 
 <template>
   <div v-if="settype !== 'defo'" class="flex w-14 justify-end border-l-[1px] border-gray-400" title="ここの設定を使用">
-    <button @click="onClick()" :class="actset()" :title="actStatus ? 'この設定を使用' : '上位設定を使用'">
+    <button
+      @click="onClick()"
+      :class="
+        MakeClassString(
+          'w-10 rounded-full border px-1 font-medium hover:bg-gray-600 hover:text-gray-100',
+          actStatus
+            ? typeColor[props.settype].bg + ' ' + 'border-gray-800'
+            : typeColor[props.higherUpType].bg + ' ' + 'border-gray-500 text-gray-500',
+        )
+      "
+      :title="actStatus ? 'この設定を使用' : '上位設定を使用'"
+    >
       {{ actStatus ? 'ON' : 'OFF' }}
     </button>
   </div>
