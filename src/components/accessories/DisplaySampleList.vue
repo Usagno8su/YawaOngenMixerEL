b9cc868dcde3b645e9b54215944b3f2c5da7daae
Usagno8su <120317207+Usagno8su@users.noreply.github.com>
2025-10-29 21:50:00 +0900

    サムネイルクリックで拡大表示するように変更 #1575

--- src/components/accessories/DisplaySampleList.vue
+++ src/components/accessories/DisplaySampleList.vue
@@ -22,6 +22,7 @@ import { createVoiceFileEncodeSetting } from '@/utils/analysisData'
 import { DEFAULT_KYARA_TATIE_UUID } from '@/data/data'
 import { onUnmounted, ref, watch, onMounted } from 'vue'
 import DisplaySampleView from '@/components/accessories/DisplaySampleView.vue'
+import DialogRawLIstFileView from '@/components/unit/DialogRawLIstFileView.vue'
 import { MakeClassString, resizeKyaraDateDisplay } from '@/utils/analysisGeneral'
 
 // サムネイル用
@@ -29,6 +30,14 @@ const refDisplaySampleView = ref<{ [key: string]: InstanceType<typeof DisplaySam
 const refShowSingle = ref<InstanceType<typeof DisplaySampleView> | null>(null)
 // 設定変更の比較チェックのため、内容を文字列に変換して保存する変数
 const checkConf = ref<{ [key: string]: string }>({})
+
+// 原寸大用
+const refDisplayRawView = ref<{ [key: string]: InstanceType<typeof DisplaySampleView> | null }>({})
+const refShowRawSingle = ref<{ [key: string]: InstanceType<typeof DisplaySampleView> | null }>({})
+const openDialog = ref<boolean>(false)
+
+// 立ち絵の拡大表示時の背景色を保存
+const tatieBgColor = ref<string>('#ffffff')
 
 // 会話中・待機中のどちらの立ち絵を表示するか指定
 const tatieSituation = ref<tatieSituationType>('tatieUUID')
@@ -112,6 +121,50 @@ const onEncodeTatie = setInterval(() => {
   }
 }, 3000)
 
+// 拡大表示用
+// 設定された元のサイズで立ち絵画像のエンコードを実施する。
+const RawEncodeTatie = () => {
+  // 複数立ち絵が必要な場合はtatieOrderListを見る
+  if (props.settype === 'tatieOrder' || props.settype === 'seid') {
+    for (const item of reversedtatieOrderList.value) {
+      const tatieKyara = props.dateList.findIndex((e) => item.settingUUID === e.uuid)
+      const itemSituation = props.selectKyara === tatieKyara ? tatieSituation.value : item.tatieSituation
+
+      if (tatieKyara != -1) {
+        encodeSetting.value = createVoiceFileEncodeSetting(tatieKyara, props.dateList)
+
+        // キャラが未選択でなければ実行
+        if (props.selectKyara !== -1 && encodeSetting.value.tatie[itemSituation].val !== DEFAULT_KYARA_TATIE_UUID) {
+          console.log('エンコード実行')
+          refDisplayRawView.value[item.uuid].getKyaraImg(encodeSetting.value, itemSituation)
+        }
+      }
+    }
+  } else {
+    encodeSetting.value = createVoiceFileEncodeSetting(props.selectKyara, props.dateList)
+
+    // キャラが未選択でなければ実行
+    if (props.selectKyara !== -1 && encodeSetting.value.tatie[tatieSituation.value].val !== DEFAULT_KYARA_TATIE_UUID) {
+      refShowRawSingle.value.refSingle.getKyaraImg(encodeSetting.value, tatieSituation.value)
+    }
+  }
+}
+
+// 立ち絵の拡大表示を行ってもいいか判断し、問題なければ開く
+const ClickRawOpen = () => {
+  // 立ち絵リスト表示か、キャラが選択されている場合、表示を行う
+  if (showOrderList.value || showKyaraUUID.value !== null) {
+    openDialog.value = true
+  }
+}
+
+// 立ち絵の拡大表示時の動作
+// 表示画面を閉じる他に、背景色を保存
+const ClickRawClose = (bgColor: string) => {
+  tatieBgColor.value = bgColor
+  openDialog.value = false
+}
+
 // 設定通りのサイズでエンコードした立ち絵画像を保存
 const RawSaveTatie = async () => {
   // const ans = await enterSaveUint8ArrayFileData(data.value.buffer, defoDir.value)
@@ -165,7 +218,10 @@ watch(
     <div class="flex w-6 flex-col text-center text-sm" v-else>
       <div class="rounded-l-lg border-[1px] border-gray-300 bg-sky-400">待機中</div>
     </div>
-    <div class="relative flex h-38 w-72 items-center justify-center border-[1px] border-gray-400">
+    <button
+      class="relative flex h-38 w-72 items-center justify-center border-[1px] border-gray-400"
+      @click="() => ClickRawOpen()"
+    >
       <div v-show="showOrderList">
         <div v-for="(item, index) in reversedtatieOrderList" :key="item.uuid">
           <DisplaySampleView
@@ -176,6 +232,16 @@ watch(
       <div v-show="!showOrderList && showKyaraUUID !== null">
         <DisplaySampleView imgClass="max-h-[150px] w-full" ref="refShowSingle" />
       </div>
-    </div>
+    </button>
+    <DialogRawLIstFileView
+      :clickClose="(bgColor: string) => ClickRawClose(bgColor)"
+      :RawEncodeTatie="() => RawEncodeTatie()"
+      :RawSaveTatie="() => RawSaveTatie()"
+      :reversedtatieOrderList="reversedtatieOrderList"
+      :refDisplayRawView="showOrderList ? refDisplayRawView : refShowRawSingle"
+      :tatieBgColor="tatieBgColor"
+      :showOrderList="showOrderList"
+      v-if="openDialog"
+    />
   </div>
 </template>
